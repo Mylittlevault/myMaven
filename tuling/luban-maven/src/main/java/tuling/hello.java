a48abc4049b8f444822d32824c83cdb8d95e29f0
范东亮 <18306891471@163.com>
2020-07-06 15:51:11 +0800

    这是我的第一次提交的maven项目

---
+++ tuling/luban-maven/src/main/java/tuling/hello.java
@@ -0,0 +1,12 @@
+package tuling;
+
+class hello{
+	public void sayHello() {
+		System.out.println("Hello World!!!");
+	}
+
+	public static void main(String[] args) {
+		hello l = new hello();
+		l.sayHello();
+	}
+}
